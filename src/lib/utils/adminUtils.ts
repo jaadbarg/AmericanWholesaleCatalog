4a494a7a84f9732c0e0a70e4fb51b11060f28cfc
johnbarguti <jaadbarg@gmail.com>
2025-02-02 00:30:26 -0500

    update for admin login

--- src/lib/utils/adminUtils.ts
+++ src/lib/utils/adminUtils.ts
@@ -1,5 +1,5 @@
 // src/lib/utils/adminUtils.ts
-export const ADMIN_EMAILS = ['admin@americanwholesale.com']
+export const ADMIN_EMAILS = ['admin@americanwholesalers.com']
 
 export function isAdmin(email: string | undefined): boolean {
   return ADMIN_EMAILS.includes(email || '')
